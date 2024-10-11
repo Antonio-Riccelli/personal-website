64d93e5ec72372f9bb3ee4e2279cfce3a64cb55e
Antonio Riccelli <anton.riccelli@gmail.com>
2024-10-11 20:08:55 +0100

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="outline outline-2 outline-lime-300 shadow-lime-200 shadow-md p-10 rounded-lg">
+<h1 class="text-3xl hover:font-bold transition-all">Antonio Riccelli</h1>
+<h2 class="p-4"> <span class="hover:animate-ping transition">Full Stack Developer</span> || <span class="hover:animate-ping transition">IT</span> || <span class="hover:animate-ping transition">DevOps</span></h2>
+
+<ul class="w-full text-white p-5 rounded-lg">
+    <li class="bg-slate-800 rounded-lg m-2 hover:bg-slate-700 hover:shadow-sm hover:shadow-lime-300 hover:outline-lime-300 hover:outline hover:outline-2 focus:shadow-lg active:shadow-lg active:translate-y-1 transition-transform"><a class="w-full p-2 inline-block m-0" href="https://github.com/Antonio-Riccelli">Github</a></li>
+    <li class="bg-slate-800 rounded-lg m-2 hover:bg-slate-600 hover:shadow-sm hover:shadow-lime-300 hover:outline-lime-300 hover:outline hover:outline-2 focus:shadow-lg active:shadow-lg active:translate-y-1 transition-transform"><a class="w-full inline-block p-2 m-0" href="https://www.linkedin.com/in/antonioriccelli/">Linkedin</a></li>
+</ul>
+</div>
+
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.black);
+    }
+
+    :global(body) {
+        height: 100vh;
+        color: white;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+  </style>
