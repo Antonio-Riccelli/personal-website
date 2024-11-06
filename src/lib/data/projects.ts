56f61186ac589f97002bc473671db8bdcf9bf75e
Antonio Riccelli <anton.riccelli@gmail.com>
2024-11-06 20:50:07 +0000

    add projects

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -3,17 +3,17 @@ import type { Project } from "$lib/types/types";
 const projects : Project[] = [
     {
         name: "Cloud Resume Challenge",
-        description: "Cloud Resume Challenge",
+        description: "Online CV created and hosted using AWS serverless technology",
         technologies: ["AWS", "DynamoDB", "CloudFront", "CDN", "API Gateway", "Python", "Testing", "TailwindCSS", "HTML", "JavaScript", "Next.js", "CI/CD", "GitHub Actions", "Git", "GitHub"],
         url: {
             live: "https://www.antonio-riccelli-cloud-resume.link/",
             github: "https://github.com/Antonio-Riccelli/cloud-resume-challenge-frontend"
         },
-        image: "https://private-user-images.githubusercontent.com/39813213/251117525-3650fad1-5988-40ec-ab2e-71a5432a1aab.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA2NTExNDYsIm5iZiI6MTczMDY1MDg0NiwicGF0aCI6Ii8zOTgxMzIxMy8yNTExMTc1MjUtMzY1MGZhZDEtNTk4OC00MGVjLWFiMmUtNzFhNTQzMmExYWFiLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDExMDMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMTAzVDE2MjA0NlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTM2MWY5ZDg1ZDc2NTUyMDc0MGIxYmRhMTQyMDI5YjZkZmNhODA3NGYxZDMxODFhZjRiMDY4MTBmYjU2ZGZlOWQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.2bw1bx_gJ-1-PVIzveewC943fJkrMI3tmvy9nh4i-DA",
+        image: "/images/cloud-resume.gif",
     },
     {
         name: "WordPress NIfTI Format Support",
-        description: "WordPress NIfTI Format Support",
+        description: "Plug-in for WordPress to allow users to upload and display NIfTI format files (WORK IN PROGRESS)",
         technologies: ["WordPress", "PHP"],
         url: {
             github: "https://github.com/Antonio-Riccelli/wordpress-nifti-support"
@@ -21,11 +21,13 @@ const projects : Project[] = [
     },
     {
         name: "Fight for Apollo Tic Tac Toe",
-        description: "Tic Tac Toe Game",
+        description: "Rocky IV-inspired Tic Tac Toe game",
         technologies: ["React.js", "CSS", "HTML", "JavaScript"],
         url: {
-            github: "https://github.com/Antonio-Riccelli/fight-for-apollo-ticTacToe"
+            github: "https://github.com/Antonio-Riccelli/fight-for-apollo-ticTacToe",
+            live: "https://fight-for-apollo.netlify.app/"
         },
+        image: "/images/fight-for-apollo-tic-tac-toe.gif"
     }
 
 ]
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,38 @@
-<h1>WORK IN PROGRESS 👷‍♂️⚒️</h1>
+<script>
+	import { Card, Button, Toggle } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+	import projects from '$lib/data/projects';
+</script>
+
+<div class="w-full mt-24 mb-24 text-center">
+	<h1 class="font-extrabold">Projects</h1>
+	<div class="flex flex-wrap justify-center dark:text-white text-white">
+		{#each projects as { name, description, technologies, url, image }}
+			<Card class="max-w-sm m-5 p-0 dark:text-white h-full" img={image} imgClass="h-72">
+				<h2 class="font-bold p-2">{name}</h2>
+				<p class="p-2 text-start">{description}</p>
+				<div class="my-1 text-start">
+					{#if url?.live}
+						<Button href={url?.live} class="mx-1">
+							Live <ArrowRightOutline />
+						</Button>
+					{/if}
+					{#if url?.github}
+						<Button href={url.github} class="mx-1">
+							GitHub <ArrowRightOutline />
+						</Button>
+					{/if}
+				</div>
+				<div>
+					<ul class="list-none p-0 m-0 flex flex-wrap">
+						{#each technologies as technology}
+							<li class="p-2 mx-1 my-2 inline-block dark:bg-blue-600 rounded-lg">
+								{technology}
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</Card>
+		{/each}
+	</div>
+</div>
