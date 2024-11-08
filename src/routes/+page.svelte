8c98b5c375aac40fcc38c1c77f8c6b5de7e1d2a8
Antonio Riccelli <anton.riccelli@gmail.com>
2024-11-08 22:17:08 +0000

    update light mode palette

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   </script>
 
-<main class="flex mt-26 mb-20 container mx-auto px-4 align-middle items-center justify-center h-full">
+<main class="flex mt-26 mb-20 w-full align-middle items-center justify-center h-full lightmode-bg dark:bg-black">
 <div class="outline outline-3 outline-black shadow-black dark:outline-blue-600 dark:shadow-blue-600 shadow-lg p-10 rounded-lg overflow-auto">
 <h1 class="text-3xl hover:font-bold transition-all">Antonio Riccelli</h1>
 <h2 class="p-4"> <span class="hover:animate-ping transition">Full Stack Developer</span> || <span class="hover:animate-ping transition">IT</span> || <span class="hover:animate-ping transition">DevOps</span></h2>
@@ -11,10 +11,6 @@
    <img src="/images/pp-4.webp" class="dark:block hidden p-8 rounded-full hover:animate-bounce" alt="profile" />
    <img src="/images/pp-3.png" class="dark:hidden block p-8 rounded-full hover:animate-bounce" alt="profile" />
 </div>
-<!-- <ul class="w-full text-white p-5 rounded-lg">
-    <li class="bg-slate-800 rounded-lg m-2 hover:bg-slate-700 hover:shadow-sm hover:shadow-lime-300 hover:outline-lime-300 hover:outline hover:outline-2 focus:shadow-lg active:shadow-lg active:translate-y-1 transition-transform"><a class="w-full p-2 inline-block m-0" href="https://github.com/Antonio-Riccelli">Github</a></li>
-    <li class="bg-slate-800 rounded-lg m-2 hover:bg-slate-600 hover:shadow-sm hover:shadow-lime-300 hover:outline-lime-300 hover:outline hover:outline-2 focus:shadow-lg active:shadow-lg active:translate-y-1 transition-transform"><a class="w-full inline-block p-2 m-0" href="https://www.linkedin.com/in/antonioriccelli/">Linkedin</a></li>
-</ul> -->
 </main>
 
 <style lang="postcss">
